e6bdd3a5bc8a4413bd426cb90b4dba98cc9595ee
Csenge Petak <csenge.petak@uvm.edu>
2023-02-02 11:33:09 -0500

    updating parameters

--- parameters.sh
+++ parameters.sh
@@ -11,9 +11,14 @@ gpulaunch2 --runs=10 basic.py -num_generations 37500 -season_len 500 -truncation
 gpulaunch2 --runs=10 basic.py -num_generations 3750 -season_len 50 -truncation_prop 0.05
 gpulaunch2 --runs=10 basic.py -num_generations 22500 -season_len 300 -truncation_prop 0.05
 gpulaunch2 --runs=10 basic.py -num_generations 37500 -season_len 500 -truncation_prop 0.05
+
 gpulaunch2 --runs=10 basic.py -num_generations 3750 -season_len 50 -mut_rate 0.05
 gpulaunch2 --runs=10 basic.py -num_generations 22500 -season_len 300 -mut_rate 0.05
 gpulaunch2 --runs=10 basic.py -num_generations 37500 -season_len 500 -mut_rate 0.05
 gpulaunch2 --runs=10 basic.py -num_generations 3750 -season_len 50 -mut_rate 0.2
 gpulaunch2 --runs=10 basic.py -num_generations 22500 -season_len 300 -mut_rate 0.2
 gpulaunch2 --runs=10 basic.py -num_generations 37500 -season_len 500 -mut_rate 0.2
+
+gpulaunch2 --runs=10 basic_lineages.py -num_generations 22500 -season_len 300
+
+gpulaunch2 --runs=10 basic_lineages.py -num_generations 22500 -season_len 300 -truncation_prop 0.05
